9e4c2d4162b3debfcc3b47b1d95426f52b2ed5d3
Caroline Ramos <carolbeatrice@gmail.com>
2024-04-11 20:09:27 -0300

    Criação de classe, atributos e metodos iniciais para ContaBancária

---
+++ ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos
+{
+    class ContaBancaria
+    {
+        public string Titular;
+
+        public double Saldo; //usa-se double por ter precisao dupla enquanto float é ponto flutuante de precisão simples, evitando dizima periodica
+        
+        //método construtor
+        public ContaBancaria(string titular, double saldoInicial)
+        {
+            Titular = titular;
+            Saldo = saldoInicial;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor; //soma o valor do deposito ao saldo existente;
+            Console.WriteLine($"Deposito de {valor} realizado com sucesso.");
+        }
+
+        public void Sacar(double valor){
+            if (valor>Saldo)
+            {
+                Console,WriteLine("Saldo insuficiente.");
+            }
+                Saldo =- valor;
+                Console.WriteLine($"Saque de {valor} realizado com sucesso");
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo atual da conta de {Titular}: {Saldo}");
+        }
+
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Criando instancia da conta
+            ContaBancaria contaJoao = new ContaBancaria("João", 1000);
+
+            //Realizando operações na conta criada
+            contaJoao.Depositar(500);
+            contaJoao.Sacar(200);
+            contaJoao.ExibirSaldo();
+
+        }
+    }
+}
